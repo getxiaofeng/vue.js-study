6c75257673dd73ed7306856d000ca04800fb7531
getxiaofeng <45992347+getxiaofeng@users.noreply.github.com>
2019-04-12 23:30:27 +0800

    Enter

---
+++ 5v-model及其修饰符/js/main.js
@@ -0,0 +1,7 @@
+var app=new Vue({
+    el:'#app',
+    data: {
+        name:'feng',
+        price:'10',
+    }
+})
---
+++ 6v-model在其他元素及类型的用法/js/main.js
@@ -0,0 +1,24 @@
+var app=new Vue({
+    el: '#app',
+    data:{
+        sex: "male",
+    }
+})
+var bpp=new Vue({
+    el: '#bpp',
+    data:{
+        sex: [],
+    }
+})
+var cpp=new Vue({
+    el: '#cpp',
+    data:{
+        from: 1,
+    }
+})
+var dpp=new Vue({
+    el: '#dpp',
+    data:{
+        des: [],
+    }
+})
