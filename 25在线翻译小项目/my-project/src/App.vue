4aa16ee2a33dfbbd2552189641c56b7e1cf99450
getxiaofeng <l>
2019-04-17 21:35:12 +0800

    ..

---
+++ 25在线翻译小项目/my-project/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h1>在线翻译</h1>
+    <h5  class="text-muted" >简单 / 易用 / 便捷</h5>
+    <translateFrom v-on:fromSubmit="transText"></translateFrom>
+    <translateOutput v-text="translatedText"></translateOutput>
+  </div>
+</template>
+
+<script>
+
+import TranslateFrom from './components/TranslateFrom'
+import TranslateOutput from './components/TranslateOutput'
+export default {
+  name: 'App',
+  data:function(){
+    return{
+      translateText:""
+    }
+  },
+  components: {
+    TranslateFrom,TranslateOutput
+  },
+  methods:{
+    transText:function(text,language){
+     // alert(text);
+     this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170721T082515Z.54cf3dc583f679db.f4a96182281281d8b5dfe24b4e88298e2133f219&lang='+language+'&text='+text)
+      then((response)=>{
+        //console.log(response.body.text[0])
+        this.translatedText=response.body.text[0];
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 25在线翻译小项目/my-project/src/components/TranslateFrom.vue
@@ -0,0 +1,48 @@
+<template>
+  <div  id="translateFrom">
+    <div >
+      <form id="transForm"  v-on:submit="formSubmit">
+        <input class="form-control" type="text" v-model="textToTranslate"  placeholder="输入需要翻译的内容">
+            <select class="form-control" v-model="language">
+                <option value="en">English</option>
+                <option value="ru">Russian</option>
+                <option value="ko">Korean</option>
+                <option value="ja">Janpenese</option>
+            </select>
+        <input  type="submit" value="翻译">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'translateFrom',
+  data:function(){
+      return{
+          textToTranslate:"",
+          language:""
+      }
+  },
+  methods:{
+      formSubmit:function(e){
+         //alert(this.textToTranslate);
+         this.$emit("fromSubmit",this.textToTranslate,this.language);
+         e.preventDefault();
+      }
+  },
+  created:function(){
+      this.language="en";
+  }
+  
+}
+</script>
+#transForm {
+    border:1px solid #ccc;
+    border-radius:10px;
+}
+<style>
+
+</style>
